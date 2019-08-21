15d1882a9a952bd958f5404fb26e2f48d82ab1d6
Andrew Williams <1williamsaj@gmail.com>
2019-08-21 02:19:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index) #First, we define a method with 2 arguments
-  #Next, we create our conditional.
-  if position_taken?(board,index) == false && index.to_i.between?(0, 8)
-    return true
+  #Next, we create our conditional. It first checks the 'position_taken?' method to see if the space on the board is open.
+  if position_taken?(board,index) == false && index.to_i.between?(0, 8) #Then, it also checks under the 'and' operator the index number to see if it is a valid board place.
+    return true #If both conditions are met, the conditional returns true. If not, a falsey value will be returned.
   end
 
 end
